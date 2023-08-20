51179e4c2fd6589441a70255c85e35d265c7554b
6ixfalls <23470032+6ixfalls@users.noreply.github.com>
2023-08-20 02:03:00 -0700

    Question object

--- src/components/mobile-nav.tsx
+++ src/components/mobile-nav.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import * as React from "react";
 import Link, { LinkProps } from "next/link";
 import { useRouter } from "next/navigation";
---
+++ src/components/question-card.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import {
+    Card,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { ChevronRightIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+import Tilt from "react-parallax-tilt";
+
+export function QuestionCard(props: {
+    title: string;
+    content: string;
+    id: string;
+}) {
+    return (
+        <Tilt
+            transitionSpeed={500}
+            tiltMaxAngleX={8}
+            tiltMaxAngleY={4}
+            scale={1.02}
+            tiltReverse
+            glareEnable
+            glareBorderRadius="0.75rem"
+            glareMaxOpacity={0.17}
+        >
+            <Link href={"/questions/" + props.id}>
+                <Card className="border border-input bg-transparent shadow-sm hover:text-accent-foreground">
+                    <CardHeader>
+                        <CardTitle>
+                            {props.title}
+                            <ChevronRightIcon className="ml-auto h-6 w-6" />
+                        </CardTitle>
+                        <CardDescription>{props.content}</CardDescription>
+                    </CardHeader>
+                </Card>
+            </Link>
+        </Tilt>
+    );
+}
