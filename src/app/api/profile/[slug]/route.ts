6ec416169b66529f3a9ef0d56d26b426bdf9634f
6ixfalls <23470032+6ixfalls@users.noreply.github.com>
2023-08-20 21:25:54 -0700

    Add missing rep

--- src/app/api/profile/[slug]/route.ts
+++ src/app/api/profile/[slug]/route.ts
@@ -1,5 +1,7 @@
-import { firestore } from "@/app/api/auth/[...nextauth]/route";
 import { NextRequest, NextResponse } from "next/server";
+import { authOptions, firestore } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+import { DocumentReference } from "firebase-admin/firestore";
 
 export type User = {
     name: string;
@@ -33,4 +35,55 @@ export async function GET(
     });
 }
 
-export async function POST(request: NextRequest, response: NextResponse) {}
+export async function POST(
+    request: NextRequest,
+    { params }: { params: { slug: string } }
+) {
+    const session = await getServerSession(authOptions);
+    if (!session)
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+
+    const user = firestore.doc(`users/${encodeURIComponent(params.slug)}`);
+    if (user.id === session.user.id)
+        return NextResponse.json(
+            { error: "You cannot upvote yourself." },
+            { status: 400 }
+        );
+    const userObject = await user.get();
+    if (!userObject.exists)
+        return NextResponse.json({ error: "User not found" }, { status: 404 });
+    const userData = userObject.data();
+    if (!userData)
+        return NextResponse.json({ error: "User not found" }, { status: 404 });
+    if (
+        userData.upvoters &&
+        userData.upvoters.find(
+            (u: DocumentReference) => u.id === session.user.id
+        )
+    )
+        return NextResponse.json(
+            { error: "You have already upvoted this user." },
+            { status: 400 }
+        );
+
+    const newRep = (userData.reputation || 0) + 1;
+
+    await user.update({
+        reputation: newRep,
+        upvoters: [
+            ...(userData.upvoters || []),
+            firestore.doc(`users/${session.user.id}`),
+        ],
+    });
+
+    return NextResponse.json({
+        user: {
+            name: userData.name,
+            image: userData.image,
+            bio: userData.bio,
+            tags: userData.tags || [],
+            id: userObject.id,
+            reputation: newRep,
+        },
+    });
+}
